87e8306fe7e822bb06844ad86da02ee422f617c6
Ahmettanr7 <Ahmet Tanrkulu@AHMETTAN>
2021-04-13 16:17:54 +0300

    8. Gün Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,23 +1,23 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //S(O)LID
+    //Open Closed Principle - Yeni özellik eklendiğinde bozulmama
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
-                Console.WriteLine( "Kategori : {0}  Ürün : {1}  Fiyat : {2}  Stok Durumu : {3}",
-                   product.CategoryId ,
-                   product.ProductName, 
-                   product.UnitPrice, 
-                   product.UnitsInStock
-                    );
+                Console.WriteLine( "Kategori : {0} - Ürün : {1} - Fiyat : {2} - Stok Durumu : {3}",
+                   product.CategoryId ,product.ProductName, product.UnitPrice, product.UnitsInStock
+                                  );
             };
         }
     }
